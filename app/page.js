efc5e0e3ec894894aafb19bb907dc6d102e80107
Sandeep Kadam <sandeepkadam1798.sk@gmail.com>
2024-08-25 13:48:34 +0530

    new chnages

--- app/page.js
+++ app/page.js
@@ -10,9 +10,10 @@ import Image from "next/image";
 export default function Home() {
   return (
    <main className="font-sans text-gray h-screen ">
-    <BasicDemo/>
+    
 
     <Categorynav />
+    <BasicDemo/>
    </main>
 
   );
