0d5f48bc27c821250ae702291d592bc588e32a13
Santoshalimkar <santosh.sa4d@gmail.com>
2024-09-30 00:02:13 +0530

    Changes added

--- app/providers.js
+++ app/providers.js
@@ -14,7 +14,7 @@ export function NextuiProviderWrapper({children}) {
 
   useEffect(() => {
     const token = Cookies.get('token');
-    if (!token) {
+    if (token) {
       setIsAuthenticated(true);
       if (pathname === '/Signin') {
         router.push('/');
