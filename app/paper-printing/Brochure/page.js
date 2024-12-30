a2c4e6deada9715315ee019e8e41d9d3ab452fe9
Sandeep Kadam <sandeepkadam1798.sk@gmail.com>
2024-12-30 11:31:24 +0530

    Working on Brochure

---
+++ app/paper-printing/Brochure/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Brochure from './Brochure'
+
+const page = () => {
+  return (
+    <div className='bg-[#f1f2f4]'>
+
+    <Brochure/>
+    {/* <RecentlyView/> */}
+
+</div>
+  )
+}
+
+export default page
