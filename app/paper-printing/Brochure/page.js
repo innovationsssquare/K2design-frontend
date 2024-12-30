18ad3721ccd8c05f716421666af049992a755690
Sandeep Kadam <sandeepkadam1798.sk@gmail.com>
2024-12-30 21:55:38 +0530

    Working on Booklet

--- app/paper-printing/Brochure/page.js
+++ app/paper-printing/Brochure/page.js
@@ -1,12 +1,19 @@
+import Booklet from '@/components/Paper-printing/Booklet/Booklet'
+import Brochure from '@/components/Paper-printing/Brochure/Brochure'
+import DigitalPoster from '@/components/Paper-printing/DigitalPoster/DigitalPoster'
+import Stamp from '@/components/Paper-printing/Stamp/Stamp'
 import React from 'react'
-import Brochure from './Brochure'
+
 
 const page = () => {
   return (
     <div className='bg-[#f1f2f4]'>
 
-    <Brochure/>
-    {/* <RecentlyView/> */}
+    {/* <Brochure/> */}
+    {/* <Booklet/> */}
+    <DigitalPoster/>
+    {/* <Stamp/> */}
+  
 
 </div>
   )
